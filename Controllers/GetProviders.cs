18575d3830b1a1559ca4a7656bb97937d4996db2
Misaka <misaka@nekoit.org>
2025-02-23 16:39:59 +0800

    Update

--- Controllers/GetProviders.cs
+++ Controllers/GetProviders.cs
@@ -1,6 +1,7 @@
 using LLMRolePlay.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LLMRolePlay.Controllers
 {
@@ -15,10 +16,27 @@ namespace LLMRolePlay.Controllers
 
       User? user = await Models.User.GetUserByToken(_dBContext, token);
       if (user == null) return ApiResponse.TokenError();
-      var providers = _dBContext.Providers.Where(provider => provider.UserId == user.Id).ToList();
+      var providers = _dBContext.Providers.Include(provider => provider.Models).Where(provider => provider.UserId == user.Id).ToList();
       return ApiResponse.Success(new
       {
-        providers = providers
+        providers = providers.Select(provider => new
+        {
+          id = provider.Id,
+          name = provider.Name,
+          description = provider.Description,
+          baseUrl = provider.BaseUrl,
+          apiKey = provider.ApiKey,
+          type = provider.Type,
+          settings = provider.Settings,
+          models = provider.Models.Select(model => new
+          {
+            id = model.Id,
+            name = model.Name,
+            modelName = model.ModelName,
+            settings = model.Settings,
+            description = model.Description
+          })
+        })
       });
     }
   }
