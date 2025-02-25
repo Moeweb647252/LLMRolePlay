e3347ff424f952e562328e9c8150378622e34492
Misaka <misaka@nekoit.org>
2025-02-25 21:16:14 +0800

    Update

--- Providers/ChatMessage.cs
+++ Providers/ChatMessage.cs
@@ -1,6 +1,12 @@
-namespace LLMRolePlay.Models {
-    public class ChatMessage {
-        public required string Content { get; set; }
-        public required string Role { get; set; }
-    }
+using System.Text.Json.Serialization;
+
+namespace LLMRolePlay.Models
+{
+  public class ChatMessage
+  {
+    [JsonPropertyName("content")]
+    public required string Content { get; set; }
+    [JsonPropertyName("role")]
+    public required string Role { get; set; }
+  }
 }
--- Providers/OpenAI.cs
+++ Providers/OpenAI.cs
@@ -1,25 +1,93 @@
 using LLMRolePlay.Models;
+using Newtonsoft.Json;
+using System.Collections;
+using System.Net.Http.Headers;
+using System.Text.Json;
 
-namespace LLMRolePlay.Providers {
-    public class OpenAISettings {
-        public double? temperature;
-        public ulong? max_tokens;
-        public double? top_p;
+namespace LLMRolePlay.Providers
+{
+  public class OpenAISettings
+  {
+    public double? temperature;
+    public ulong? max_tokens;
+    public double? top_p;
+  }
+
+  public class OpenAIUpdate
+  {
+    public string? delta { get; set; }
+  }
+
+  public class OpenAICompletionRequest
+  {
+    public required string model { get; set; }
+    public required IEnumerable<ChatMessage> messages { get; set; }
+    public bool stream { get; } = true;
+    public double? temperature { get; set; }
+    public ulong? max_tokens { get; set; }
+    public double? top_p { get; set; }
+
+  }
+  public class OpenAIStreamingResponseChunkChoiceDelta
+  {
+    public string? content { get; set; }
+    public string? role { get; set; }
+    public string? refusal { get; set; }
+    public ulong? index { get; set; }
+  }
+
+  public class OpenAIStreamingResponseChunkChoice
+  {
+    public required OpenAIStreamingResponseChunkChoiceDelta delta { get; set; }
+    public required string id { get; set; }
+    public required ulong created { get; set; }
+    public required string model { get; set; }
+  }
+
+  public class OpenAIStreamingResponseChunk
+  {
+    public string? id { get; set; }
+
+  }
+
+  public class OpenAI
+  {
+    public required Model model { get; set; }
+
+    public OpenAI(Model model)
+    {
+      this.model = model;
     }
 
-    public class OpenAI {
-        public required string apiKey;
-        public required string baseUrl;
+    public async IAsyncEnumerable<OpenAIUpdate> Compeletion(IEnumerable<ChatMessage> messages)
+    {
+      var client = new HttpClient();
+      client.DefaultRequestHeaders.Add("Authorization", $"Bearer {model.Provider.ApiKey}");
+      var request = new HttpRequestMessage(HttpMethod.Post, model.Provider.BaseUrl + "/v1/chat/completions");
+      OpenAISettings? settings = JsonConvert.DeserializeObject<OpenAISettings>(model.Settings);
+      request.Content = new StringContent(JsonConvert.SerializeObject(new OpenAICompletionRequest
+      {
+        model = model.ModelName,
+        messages = messages,
+        temperature = settings?.temperature,
+        max_tokens = settings?.max_tokens,
+        top_p = settings?.top_p
+      }));
+      request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+      var response = await client.SendAsync(request);
+      if (response.IsSuccessStatusCode)
+      {
+        var stream = await response.Content.ReadAsStreamAsync();
+        using var reader = new StreamReader(stream);
+        while (!reader.EndOfStream)
+        {
+          string? line = await reader.ReadLineAsync();
+          if (line != null)
+          {
 
-        public OpenAI(string apiKey, string baseUrl) {
-            this.apiKey = apiKey;
-            this.baseUrl = baseUrl;
+          }
         }
-
-        public async Task Compeletion(string model, IEnumerable<ChatMessage> messages, OpenAISettings settings) {
-            var client = new HttpClient();
-            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
-            HttpRequest request = 
-        }
+      }
     }
+  }
 }
