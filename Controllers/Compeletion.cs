70110b2840d652226517ecd8a41b5f43a03b2d82
Misaka <misaka@nekoit.org>
2025-02-26 13:55:03 +0800

    Update

--- Controllers/Compeletion.cs
+++ Controllers/Compeletion.cs
@@ -3,30 +3,52 @@ using LLMRolePlay.Providers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Newtonsoft.Json;
 
 namespace LLMRolePlay.Controllers {
     
     public class CompeletionRequest {
-        public required uint modelId;
-        public required uint chatId;
+        public required uint participantId;
     }
     public partial class API : ControllerBase {
         [HttpPost("compeletion")]
         [AllowAnonymous]
-        public async Task Compeletion(HttpContext ctx, [FromBody] CompeletionRequest data) {
+        public async Task Compeletion([FromBody] CompeletionRequest data) {
         string? token = Request.Headers["token"];
             if (token == null) return;
             User? user = await Models.User.GetUserByToken(_dBContext, token);
             if (user == null) return;
-            Model? model = await Model.GetModelById(_dBContext, data.modelId);
-            if (model == null) return;
-            if (model.Provider.UserId != user.Id && !model.IsPublic) return;
-            Chat? chat = await _dBContext.Chats.Include(c=>c.Messages).FirstOrDefaultAsync(c=>c.Id == data.chatId);
+            Participant? participant = await _dBContext.Participants.FirstOrDefaultAsync(p=>p.Id == data.participantId);
+            if (participant == null) return;
+            Model model = participant.Model;
+            Chat? chat = await _dBContext.Chats.Include(c=>c.Messages).FirstOrDefaultAsync(c=>c.Id == participant.ChatId);
             if (chat == null) return;
-            if (chat.UserId != user.Id) return;
             if (model.Provider.Type == "openai") {
-                var settings = JsonConvert.DeserializeObject<OpenAISettings>(model.Provider.Settings);
+                var openai = new OpenAI(model);
+                var content = "";
+                var stream = openai.Compeletion(chat.Messages.Select(m=> new ChatMessage {
+                    Content = m.Content,
+                    Role = m.Role,
+                })).GetAsyncEnumerator();
+                while (true) {
+                    var task = stream.MoveNextAsync().AsTask();
+                    if(await Task.WhenAny(Task.Delay(30000), task) == task) {
+                        if (task.Result) {
+                            var chunk = stream.Current;
+                            string? delta = chunk.choices?.FirstOrDefault()?.delta?.content;
+                            if (delta != null) {
+                                content += delta;
+                            }
+                        } else {
+                            break;
+                        }
+                    } else {
+                        await Response.WriteAsync(":LLMRolePlay Processing");
+                    }
+                }
+                var newMessage = new Message("assistant", content, 0, chat.Id, participant.Id);
+                _dBContext.Messages.Add(newMessage);
+                await _dBContext.SaveChangesAsync();
+                await Response.WriteAsync("[DONE]");
             }
         }
     }
